1e89cbe85f0a2d970fc223dbe04ec482ee18cceb
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-06 11:30:21 +0100

    Added #top_up method with limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,8 +2,21 @@ class Oystercard
   attr_reader :balance
 
   DEFAULT_BALANCE = 0
+  TOP_UP_LIMIT = 90
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+  end
+
+  def top_up(amount)
+    limit?(amount)
+    @balance += amount
+  end
+
+  private 
+
+  def limit?(amount)
+    message = "The top up limit is #{TOP_UP_LIMIT}"
+    raise message if @balance + amount > TOP_UP_LIMIT
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,16 @@ describe Oystercard do
   it "should have a balance" do
     expect(subject.balance).to eq(Oystercard::DEFAULT_BALANCE)
   end
+
+  describe "#top_up method" do
+    it "should top up by given amount" do
+      expect { subject.top_up(10) }.to change { subject.balance }.by(10)
+    end
+
+    it 'should raise error when balance limit is going to be passed' do 
+      message = "The top up limit is #{Oystercard::TOP_UP_LIMIT}"
+      subject.top_up(Oystercard::TOP_UP_LIMIT)
+      expect { subject.top_up(1) }.to raise_error message
+    end
+  end
 end

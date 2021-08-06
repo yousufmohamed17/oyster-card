cfea4893053e157556c39b767bcc264c36b3024f
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-06 11:51:22 +0100

    Added #touch_in, #touch_out and #in_journey? methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :entry_station, :exit_station
 
   DEFAULT_BALANCE = 0
   TOP_UP_LIMIT = 90
+  MINIMUM_BALANCE = 1
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @entry_station = nil
+    @exit_station = nil
   end
 
   def top_up(amount)
@@ -17,10 +20,29 @@ class Oystercard
     @balance -= fare
   end
 
+  def in_journey?
+    @entry_station != nil
+  end
+
+  def touch_in(station)
+    check_balance
+    @entry_station = station
+    @exit_station = nil
+  end
+
+  def touch_out(station)
+    @entry_station = nil
+    @exit_station = station
+  end
+
   private 
 
   def limit?(amount)
     message = "The top up limit is #{TOP_UP_LIMIT}"
     raise message if @balance + amount > TOP_UP_LIMIT
+  end
+
+  def check_balance
+    raise "You must top up" unless @balance >= MINIMUM_BALANCE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,11 @@
 require "oystercard"
 
 describe Oystercard do
-  it "should have a balance" do
-    expect(subject.balance).to eq(Oystercard::DEFAULT_BALANCE)
+  before { subject.top_up(20) }
+  let(:station) { double("station") }
+
+  it "new cards should have a default balance" do
+    expect(Oystercard.new.balance).to eq(Oystercard::DEFAULT_BALANCE)
   end
 
   describe "#top_up method" do
@@ -11,15 +14,55 @@ describe Oystercard do
     end
 
     it 'should raise error when balance limit is going to be passed' do 
-      message = "The top up limit is #{Oystercard::TOP_UP_LIMIT}"
-      subject.top_up(Oystercard::TOP_UP_LIMIT)
-      expect { subject.top_up(1) }.to raise_error message
+      msg = "The top up limit is #{Oystercard::TOP_UP_LIMIT}"
+      expect { subject.top_up(Oystercard::TOP_UP_LIMIT) }.to raise_error msg
     end
   end
 
   describe "#deduct" do
     it "should deduct by fare" do
       expect { subject.deduct(5) }.to change { subject.balance }.by(-5)
+    end
+  end
+
+  describe "#in_journey?" do
+    it "should return true after touch in" do
+      subject.touch_in(station)
+      expect(subject.in_journey?).to eq(true)
+    end
+
+    it "should return false after touch out" do
+      subject.touch_out(station)
+      expect(subject.in_journey?).to eq(false)
+    end
+  end
+
+  describe "#touch_in" do
+    it "should raise error if balance < #{Oystercard::MINIMUM_BALANCE}" do
+      message = "You must top up"
+      expect { Oystercard.new.touch_in(station) }.to raise_error message
+    end
+
+    it "should record entry station" do
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq(station)
+    end
+
+    it "should set exit_station to nil" do
+      subject.touch_in(station)
+      expect(subject.exit_station).to eq(nil)
+    end
+  end
+
+  describe "#touch_out" do
+    it "should record exit station" do
+      subject.touch_out(station)
+      expect(subject.exit_station).to eq(station)
+    end
+
+    it "should set entry_station to nil" do
+      subject.touch_out(station)
+      expect(subject.entry_station).to eq(nil)
     end
   end
 end
